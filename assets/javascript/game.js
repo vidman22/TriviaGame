a27cb4a24891f770579faf0fb335ff1b1d1844ce
vidman22 <jvidmar37@gmail.com>
2017-09-01 12:04:01 -0700

    TriviaGame

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+
 	
 // Timeout Functions
 	var number = 30;
@@ -5,9 +6,84 @@
     var intervalId;
 
 
-    function run() {
+    $("#display").one("click", function() {
       intervalId = setInterval(decrement, 1000);
-    }
+      
+      // Display the questions
+	$(".question").html('<p>How many rings to rule them all?</p>'+
+		
+		'<br>' + 
+ 			'<input type="radio" id="q1" name="ring" value="0" >0'+
+ 			'<input type="radio" id="q2" name="ring" value="1" >1'+
+			'<input type="radio" id="q3" name="ring" value="2" >2'+
+		
+
+		'<br><br><br>'+
+		
+
+		
+		'<p>How many armies were present at the Battle of Five Armies?</p>'+
+		
+		'<br>'+
+
+  			'<input type="radio" id="q4" name="battle" value="3" >3'+
+ 		    '<input type="radio" id="q5" name="battle" value="4" >4'+
+		    '<input type="radio" id="q6" name="battle" value="5" >5'+
+		
+
+		'<br><br><br>'+
+		
+
+		
+		
+		'<p>How many Seven Seeing Stones were made?</p>'+
+		
+		'<br>'+
+
+  			'<input type="radio" id="q7" name="stone" value="6" >6'+
+  			'<input type="radio" id="q8" name="stone" value="7" >7'+
+ 			'<input type="radio" id="q9" name="stone" value="8" >8'+
+		
+		'<br><br><br>'+
+
+		
+		
+		
+		'<p>How many of the Nine Nazgul were present at the Battle of Pellenor Fields?</p>'+
+		
+		'<br>'+
+
+  			'<input type="radio" id="q10" name="nine" value="9" >9'+
+  			'<input type="radio" id="q11" name="nine" value="10" >10'+
+ 			'<input type="radio" id="q12" name="nine" value="11" >11'+
+		
+
+
+		
+		'<br><br><br>'+
+
+		
+		'<p>How many children were in the dozen that Samwise had with Rosie?</p>'+
+		
+		'<br>'+
+		
+  			'<input type="radio" id="q13" name="children" value="12" >12'+
+ 			'<input type="radio" id="q14" name="children" value="13" >13'+
+ 			'<input type="radio" id="q15" name="children" value="14" >14'+
+		
+		
+
+		'<br><br><br>'+
+
+		
+
+	
+		 '<input name="Submit" type="submit" onClick="JavaScript:handleClick()">');
+    
+
+
+    });
+    
 
     function decrement() {
 
@@ -20,6 +96,7 @@
         stop();
 
         alert("Time Up!");
+        return;
       }
 
       function stop() {
@@ -30,17 +107,24 @@
     }
 
 
-    run();
+   
 	var values = [];
 	var answers = ["1", "5", "7", "9", "12"];
-	var wins = 0;
-
+	var matches = 0;
+	var unmatched = 0;
 	console.log(answers);
 
   function handleClick() {
    
+  	// Stop the timer
+  	
+  	function stop() {
 
-	
+      clearInterval(intervalId);
+    }
+    stop();
+
+    // Check the radio button inputs
 
 	var question1Value = $("input[name=ring]:checked").val();
 	values.push(question1Value);
@@ -57,18 +141,32 @@
 	var question2Value = $("input[name=children]:checked").val();
 	values.push(question2Value);
 
+	// Loop through answers and correct answers
+
 	for( i = 0; i < answers.length; i++) { 
 
 		if (values[i] === answers[i]) {
-		wins++;
-		console.log(wins);
+		matches++;
 
-		}
-		
 	}
-		alert("You got " + wins + "of 5 correct!");
+		if(values[i] ===! answers[i]) {
+		unmatched++;
+		}
+	}
+		console.log(matches);
+		console.log(unmatched);
+
+		var unaswered = answers.length - (matches + unmatched);
+		var incorrect = answers.length - 
+
+		$(".question").html(
+			"<h2>Done!</h2>"+
+			"You got " + matches + " of 5 correct!");
+							
+
 	}
 	
+
 
 console.log(values);
 
